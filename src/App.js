7706acedae4b486375742bed9b51d4a6cf1755d1
Ranajit555 <ranajitmallick555@gmail.com>
2024-03-16 11:07:41 +0530

    header color change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import Header_box from "./components/basics/Header_box";
 import Navbar from "./components/basics/Navbar";
 import Containt from "./components/basics/Containt_box";
-import {Footer} from "./components/basics/Footer_box"
-const App=()=>{
+import { Footer } from "./components/basics/Footer_box";
+const App = () => {
   return (
     <div className="h-screen text-[18px] text-black text-opacity-100">
-      <div id="Header_box" className="fixed h-[17%] w-[100%] bg-white">
-        <Header_box/><hr/>
-        <Navbar/> 
+      <div id="Header_box" className="fixed h-[17%] w-[100%] bg-[rgb(0,113,220)]">
+        <Header_box />
+        <hr />
+        <Navbar />
       </div>
       <div id="Container" className=" h-[fit] w-[100%]">
         <div id="containt" className="h-[fit] ml-[20vh] mr-[19vh]">
-          <Containt/>
+          <Containt />
         </div>
       </div>
       <div id="Footer" className="h-[9%]">
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
-}
+};
 export default App;
--- src/components/basics/Header_box.js
+++ src/components/basics/Header_box.js
@@ -1,9 +1,9 @@
 import React from "react";
 const Header_box = () => {
-    return (
-        <div className="flex items-center h-[62%] ml-[5vh]">
-           <p className="text-[54px] text-[blue]">TRUE HEALTH</p>
-        </div>
-    );
-}
+  return (
+    <div className="flex items-center h-[62%] ml-[5vh]">
+      <p className="text-[54px] text-[white]">TRUE HEALTH</p>
+    </div>
+  );
+};
 export default Header_box;
